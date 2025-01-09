4593c8e0005ce595770102af4fffc9d59f842c1e
ma27inranma <ma27inranma@outlook.jp>
2025-01-09 13:23:13 +0900

    fix: broken break time guess for Froglight, CryingObsidian. (but just a guessed time)

--- src/main/java/cn/nukkit/block/BlockFroglight.java
+++ src/main/java/cn/nukkit/block/BlockFroglight.java
@@ -15,4 +15,9 @@ public abstract class BlockFroglight extends BlockSolid {
     public double getResistance() {
         return 0.3;
     }
+
+    @Override
+    public double getHardness() {
+        return 0.3; //TODO Without testing, this is just a guess
+    }
 }
